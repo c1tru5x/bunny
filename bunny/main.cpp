52a9d5ea012faaefb2886a8f116221943333993b
Chris <38854777+c1tru5x@users.noreply.github.com>
2019-05-30 12:38:41 +0200

    reinitialized everything
    also updated offstes for 30.MAY.2019

--- bunny/main.cpp
+++ bunny/main.cpp
@@ -5,19 +5,19 @@ CHackProcess fProcess;
 
 using namespace std;
 
-#define onGround 257
-#define crouchedGround	263
+constexpr auto onGround = 257;
+constexpr auto crouchedGround = 263;
 
-const DWORD dwPlayerBase = 0xCF2A3C;
-const DWORD dwJump = 0x51A814C;
-const DWORD entityList = 0x4D04A94;
+const DWORD dwPlayerBase = 0xCF1A4C;
+const DWORD dwJump = 0x51A715C;
+const DWORD entityList = 0x4D03AA4;
 const DWORD m_fFlags = 0x104;
 const DWORD flashAlpha = 0xA3F0;
 const DWORD isSpotted = 0x93D;
 const DWORD dwCham = 0x70;
 const DWORD dwTeam = 0xF4;
 const DWORD dwCrossID = 0xB3AC;
-const DWORD dwModelAmb = 0x58ED1C; //for brightness
+const DWORD dwModelAmb = 0x58FD1C; //for brightness
 
 bool bBhop = false;
 bool bflash = false;
@@ -202,7 +202,7 @@ int main(void)
 	}
 	else
 	{
-		cout << "Bunny by c1tru5x\nUPDATED: 23.MAI.2019" << endl;
+		cout << "Bunny by c1tru5x\nUPDATED: 30.MAY.2019" << endl;
 		cout << "F11 to close!" << endl;
 		cout << "[F1] BHOP use SPACE" << endl;
 		cout << "[F2] No Flash!" << endl;
