fdfb691c42fcc69ff4d1b7a8b0b8d60998265c40
c1tru5x <gino.chris28@gmail.com>
2019-06-19 12:22:43 +0200

    .gitignore is now working and bhop is fixed

--- bunny/main.cpp
+++ bunny/main.cpp
@@ -1,16 +1,16 @@
 #include <Windows.h>
 #include <iostream>
+#include <vector>
+#include <math.h>
 #include "connector.h"
 CHackProcess fProcess;
-
-using namespace std;
 
 constexpr auto onGround = 257;
 constexpr auto crouchedGround = 263;
 
 const DWORD dwPlayerBase = 0xCF2A3C;
-const DWORD dwJump = 0x51A812C;
-const DWORD entityList = 0x4D04A74;
+const DWORD dwJump = 0x51A81DC;
+const DWORD entityList = 0x4D04B24;
 const DWORD m_fFlags = 0x104;
 const DWORD flashAlpha = 0xA3F0;
 const DWORD isSpotted = 0x93D;
@@ -18,9 +18,12 @@ const DWORD dwCham = 0x70;
 const DWORD dwTeam = 0xF4;
 const DWORD dwCrossID = 0xB3AC;
 const DWORD dwModelAmb = 0x58FD1C; //for brightness
-const DWORD dwLeft = 0x313616C; //for auto strafe
-const DWORD dwRight = 0x3136190; //for auto strafe
+const DWORD dwLeft = 0x313624C; //for auto strafe
+const DWORD dwRight = 0x3136240; //for auto strafe
 const DWORD bSendPackets = 0xD280A; //for lag (in BYTE!)
+const DWORD dwHealth = 0x100; 
+const DWORD dwMouseEnable = 0xCF8588; //for not jumping when chatting in window
+const DWORD dwVecVelocity = 0x114;
 
 
 bool bBhop = false;
@@ -38,6 +41,10 @@ struct myPlayer_T
 	int iTeam = 0;
 	int iHealth = 0;
 	int iCrossID = 0;
+
+	std::vector<float> vel;
+
+	
 	void ReadInfo()
 	{
 		//Read this all the time..
@@ -46,6 +53,8 @@ struct myPlayer_T
 		ReadProcessMemory(fProcess.__HandleProcess, (PBYTE*)(dwLocalP + flashAlpha), &flash, sizeof(int), 0);
 		ReadProcessMemory(fProcess.__HandleProcess, (PBYTE*)(dwLocalP + dwTeam), &iTeam, sizeof(int), 0);
 		ReadProcessMemory(fProcess.__HandleProcess, (PBYTE*)(dwLocalP + dwCrossID), &iCrossID, sizeof(int), 0);
+		ReadProcessMemory(fProcess.__HandleProcess, (PBYTE*)(dwLocalP + dwHealth), &iHealth, sizeof(int), 0);
+		ReadProcessMemory(fProcess.__HandleProcess, (PBYTE*)(dwLocalP + dwVecVelocity), &vel, sizeof(vel), 0);
 	}
 }myPlayer;
 
@@ -55,8 +64,13 @@ void bunny()
 	{
 		int doJump = 5;
 		int stopJump = 4;
+		byte bMouseEnabled = 0; //88 = mouse off, 89 = mouse on
 
-		if (myPlayer.flag == onGround || myPlayer.flag == crouchedGround)
+		ReadProcessMemory(fProcess.__HandleProcess, (PBYTE*)(fProcess.__dwordClient + dwMouseEnable), &bMouseEnabled, sizeof(bMouseEnabled), 0);
+		//only when alive and moving
+		if ((myPlayer.flag == onGround && myPlayer.iHealth > 0 && bMouseEnabled == 89 && myPlayer.vel != std::vector<float>(0.0f)) ||
+			(myPlayer.flag == crouchedGround && myPlayer.iHealth > 0 && bMouseEnabled == 89 && myPlayer.vel != std::vector<float>(0.0f))
+			)
 		{
 			WriteProcessMemory(fProcess.__HandleProcess, (PBYTE*)(fProcess.__dwordClient + dwJump), &doJump, sizeof(doJump), 0);
 		}
@@ -121,7 +135,7 @@ void drawChams()
 		//no need for alpha value because brightness does the work for us
 		byte rgbColor[3]= {33, 103, 255}; //cyan
 		byte rgbColorEnemy[3] = {255, 25, 155}; //pink
-		float brightness = 65.f; //65 is very bright
+		float brightness = 15.f; //65 is very bright
 		DWORD thisPtr = (int)(fProcess.__dwordEngine + dwModelAmb - 0x2c);
 		DWORD xored = *(DWORD*)&brightness ^ thisPtr;
 		
@@ -219,19 +233,19 @@ int main(void)
 	fProcess.RunProcess();    //always forgetting this line...
 	if (fProcess.__HWNDCSgo == NULL) //if window not found
 	{
-		cout << "Process 'csgo.exe' not found!" << endl;
-		cout << "Last Error: " << GetLastError() << endl;
+		std::cout << "Process 'csgo.exe' not found!" << std::endl;
+		std::cout << "Last Error: " << GetLastError() << std::endl;
 	}
 	else
 	{
-		cout << "Bunny by c1tru5x" <<endl;
-		cout << "Updated 17.JUN.2019" << endl;
-		cout << "F11 to close!" << endl;
-		cout << "[NUM1] BHOP use SPACE" << endl;
-		cout << "[NUM2] No Flash!" << endl;
-		cout << "[NUM3] Radar" << endl;
-		cout << "[NUM4] Chams" << endl;
-		cout << "[NUM5] Trigger use ALT" << endl;
+		std::cout << "Bunny by c1tru5x" << std::endl;
+		std::cout << "Updated 19.JUN.2019" << std::endl;
+		std::cout << "F11 to close!" << std::endl;
+		std::cout << "[NUM1] BHOP use SPACE" << std::endl;
+		std::cout << "[NUM2] No Flash!" << std::endl;
+		std::cout << "[NUM3] Radar" << std::endl;
+		std::cout << "[NUM4] Chams" << std::endl;
+		std::cout << "[NUM5] Trigger use ALT" << std::endl;
 
 		while (!GetAsyncKeyState(VK_F11))
 		{
